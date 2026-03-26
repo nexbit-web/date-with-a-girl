f6525a96f72877c743c8d6ad7b0626e7ede0dae2
unknown <mihalcann@gmail.com>
2026-03-26 13:58:46 +0200

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,8 @@
 	import SignupForm from "$lib/components/signup-form.svelte";
 </script>
 <svelte:head>
-  <title>Запрошення на побачення 🌹</title>
-  <meta name="description" content="Особливе запрошення для тебе" />
-  <meta name="robots" content="noindex, nofollow" />
+  <title>bhghghgh</title>
+ 
 </svelte:head>
 <div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
 	<div class="w-full max-w-sm">
